1dc09f3c75bb47bdfe1e5e2215592712a5c08037
Bryce McGee <Bryce.McGee@gmail.com>
2022-04-21 22:45:58 -0700

    Scrapping for next project

--- info/info.go
+++
@@ -1,6 +0,0 @@
-package info
-
-const MainTitle = "BMI Calculator"
-const Separator = "__________________"
-const WeightPrompt = "Please enter your weight (kg): "
-const HeightPrompt = "Please enter your height (m): "
